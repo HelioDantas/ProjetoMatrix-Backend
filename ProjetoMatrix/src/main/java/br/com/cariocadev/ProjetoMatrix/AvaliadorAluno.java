9205d488314cc1905cd07905fc576be1ab7f9f8f
helio <helio_dantas@outlook.com>
2018-07-27 18:10:55 -0300

    Projeto finalizado

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -23,23 +23,20 @@ public class AvaliadorAluno {
 		if((testeEntrada(notas[0]))||(testeEntrada(notas[1]))|| (testeEntrada(notas[2])))
 			throw new IllegalArgumentException();
 			
-	
 		BigDecimal maior = new BigDecimal("0");
 		maior = notas[0];
-		if (maior.compareTo(notas[1]) ==-1 ){
+		if (maior.compareTo(notas[1]) ==-1 )
 			maior = notas[1];
-		}
-		if (maior.compareTo(notas[2]) ==-1 ){
+		if (maior.compareTo(notas[2]) ==-1 )
 			maior = notas[2];
-		}
-		
+
 		return maior;
 	}
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
 		if ((testeEntrada(nota1))|| (testeEntrada(nota2))|| (testeEntrada(nota3)))
 			throw new IllegalArgumentException();
-			
+		
 		BigDecimal soma2 = nota3.add(nota1.add(nota2));
 		BigDecimal media = soma2.divide(new BigDecimal("3"), 2, BigDecimal.ROUND_HALF_UP);
 		return media;
@@ -54,16 +51,15 @@ public class AvaliadorAluno {
 		BigDecimal number4 = new BigDecimal("4");
 		BigDecimal number6 = new BigDecimal("6");
 		
-		
-		if (media.compareTo(number4) ==-1 ){
+		if (media.compareTo(number4) ==-1 )
 			return "REPROVADO";
-		}else 
-			if((media.compareTo(number4) == 0) || (media.compareTo(number4) == 1) && (media.compareTo(number6) == -1)) {
+		else 
+			if((media.compareTo(number4) == 0) || (media.compareTo(number4) == 1) && (media.compareTo(number6) == -1)) 
 				return"PROVA_FINAL";
-			}else {
+			else 
 				return "APROVADO";
-			}
-	
-
+			
 	}
+
+
 }
