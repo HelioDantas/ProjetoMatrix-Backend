3612e55a7fe8767224928775d54dc39d45c1c885
helio <helio_dantas@outlook.com>
2018-07-27 17:59:24 -0300

    CalculadoraSalario resisado e retirado redundacias

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -18,68 +18,61 @@ public class CalculadoraSalario {
 		
 
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
-		/*if (notas==null)
-			throw new IllegalArgumentException();*/
+		
 	if((testeEntrada(salarioBruto)) || (testeEntrada(percentualImpostoINSS)))
 		throw new IllegalArgumentException();
 	
-		BigDecimal salarioLiquido = new BigDecimal("0");
-		BigDecimal valor = new BigDecimal("0");
-		valor = (salarioBruto.multiply(percentualImpostoINSS)).divide(new BigDecimal("100"), 2, BigDecimal.ROUND_HALF_UP);
-		salarioLiquido = salarioBruto.subtract(valor);
+		BigDecimal valor = (salarioBruto.multiply(percentualImpostoINSS)).divide(new BigDecimal("100"), 2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal salarioLiquido = salarioBruto.subtract(valor);
 		
 		return salarioLiquido;
 	}
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		//BigDecimal valor = new BigDecimal("0");
+
 		if(testeEntrada(salarioBruto))
 			throw new IllegalArgumentException();
-		
 		
 		BigDecimal primeiraFaixa = new BigDecimal(1693.72);
 		BigDecimal segundaFaixa = new BigDecimal(2822.90);
 		
-		if ((salarioBruto.compareTo(primeiraFaixa))==-1)
+		if ((salarioBruto.compareTo(primeiraFaixa)) == -1)
 			return (salarioBruto.multiply(new BigDecimal("8"))).divide(new BigDecimal("100"), 2, BigDecimal.ROUND_HALF_UP);
-	
 		else
-			if((((salarioBruto.compareTo(primeiraFaixa)) == 1)&&((salarioBruto.compareTo(segundaFaixa)) == -1)	)||((salarioBruto.compareTo(segundaFaixa))==0))
+			if((((salarioBruto.compareTo(primeiraFaixa)) == 1) && ((salarioBruto.compareTo(segundaFaixa)) == -1)) || ((salarioBruto.compareTo(segundaFaixa)) == 0))
 				 return (salarioBruto.multiply(new BigDecimal("9"))).divide(new BigDecimal("100"), 2, BigDecimal.ROUND_HALF_UP);
 			else
 				return (salarioBruto.multiply(new BigDecimal("11"))).divide(new BigDecimal("100"), 2, BigDecimal.ROUND_HALF_UP);
 	}
 
 	public BigDecimal getValorPlanoDeSaude(Integer idade) {
-		String  valorEmInteirosDoDesconto;
-		if (idade == null)
-			throw new IllegalArgumentException();
-		if(idade< 0)
+		String  valorEmStringsDoDesconto;
+		
+		if(testeEntrada(new BigDecimal(idade) ))
 			throw new IllegalArgumentException();
 		
 		if(idade>=0 && idade<=9) {
-			valorEmInteirosDoDesconto = "75";
-			
+			valorEmStringsDoDesconto = "75";	
 		}else
 			if(idade>=10&&idade<=19) {
-				valorEmInteirosDoDesconto = "112.50";
+				valorEmStringsDoDesconto = "112.50";
 			}else
 				if(idade>=20 && idade<=29) {
-					valorEmInteirosDoDesconto = "168.75";
+					valorEmStringsDoDesconto = "168.75";
 				}else
 					if (idade>=30 && idade<=39) {
-						valorEmInteirosDoDesconto = "253.13";
+						valorEmStringsDoDesconto = "253.13";
 					}else
 						if(idade>=40 && idade<=49) {
-							valorEmInteirosDoDesconto = "379.69";
+							valorEmStringsDoDesconto = "379.69";
 						}else
 							if(idade >= 50 && idade <=59 ) {
-								valorEmInteirosDoDesconto = "569.54";
+								valorEmStringsDoDesconto = "569.54";
 							}else {
-								valorEmInteirosDoDesconto = "854.30";
+								valorEmStringsDoDesconto = "854.30";
 							}
 	
-		BigDecimal str = new BigDecimal(valorEmInteirosDoDesconto).setScale(2);
-		return str;
+		BigDecimal ValorDoPlanoDeSaude = new BigDecimal(valorEmStringsDoDesconto).setScale(2);
+		return ValorDoPlanoDeSaude;
 	}
 
 
